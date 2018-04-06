5bc20de12ed4ae07ef110c6508cdfad74a01b977
floppyspace <davidsatwork2@hotmail.com>
2018-04-06 18:18:29 +0800

    Add project files.

---
+++ Assign 1 Calculator Solution/frm_calculator.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Assign_1_Calculator_Solution
+{
+    partial class frm_calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lbl_operator = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnModulus = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperator.Location = new System.Drawing.Point(65, 80);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(32, 33);
+            this.lblOperator.TabIndex = 26;
+            this.lblOperator.Text = "=";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(12, 125);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(42, 13);
+            this.lblAnswer.TabIndex = 25;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // lbl_operator
+            // 
+            this.lbl_operator.AutoSize = true;
+            this.lbl_operator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_operator.Location = new System.Drawing.Point(218, 74);
+            this.lbl_operator.Name = "lbl_operator";
+            this.lbl_operator.Size = new System.Drawing.Size(0, 20);
+            this.lbl_operator.TabIndex = 24;
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(12, 54);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(53, 13);
+            this.lblNum2.TabIndex = 23;
+            this.lblNum2.Text = "Number 2";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(12, 9);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(53, 13);
+            this.lblNum1.TabIndex = 22;
+            this.lblNum1.Text = "Number 1";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(71, 120);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 21;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(71, 6);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 20;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(71, 51);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 19;
+            // 
+            // btnModulus
+            // 
+            this.btnModulus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnModulus.Location = new System.Drawing.Point(189, 120);
+            this.btnModulus.Name = "btnModulus";
+            this.btnModulus.Size = new System.Drawing.Size(75, 23);
+            this.btnModulus.TabIndex = 18;
+            this.btnModulus.Text = "Modulus";
+            this.btnModulus.UseVisualStyleBackColor = true;
+            this.btnModulus.Click += new System.EventHandler(this.btn_modulus_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDivide.Location = new System.Drawing.Point(189, 91);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 17;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btn_divide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMultiply.Location = new System.Drawing.Point(189, 62);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 16;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btn_multiply_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubtract.Location = new System.Drawing.Point(189, 33);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtract.TabIndex = 15;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btn_subtract_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Location = new System.Drawing.Point(189, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // frm_calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 160);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lbl_operator);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.btnModulus);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frm_calculator";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblOperator;
+        internal System.Windows.Forms.Label lblAnswer;
+        internal System.Windows.Forms.Label lbl_operator;
+        internal System.Windows.Forms.Label lblNum2;
+        internal System.Windows.Forms.Label lblNum1;
+        internal System.Windows.Forms.TextBox txtAnswer;
+        internal System.Windows.Forms.TextBox txtNum1;
+        internal System.Windows.Forms.TextBox txtNum2;
+        internal System.Windows.Forms.Button btnModulus;
+        internal System.Windows.Forms.Button btnDivide;
+        internal System.Windows.Forms.Button btnMultiply;
+        internal System.Windows.Forms.Button btnSubtract;
+        internal System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Assign 1 Calculator Solution/frm_calculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+
+//  By: David Parkin
+//  Date: 5/04/2018
+//  Purpose: Simple calculator reduced down in size with functions
+//  Notes: to simplify further at some stage.. 
+
+namespace Assign_1_Calculator_Solution
+{
+    public partial class frm_calculator : Form
+    {
+        public frm_calculator()
+        {
+            InitializeComponent();
+        }
+
+        float num1 = 0, num2 = 0, result;
+       
+        //Add
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (validateInput(ref txtNum1, ref num1))
+                if (validateInput(ref txtNum2, ref num2))
+                {
+                    result = num1 + num2;
+                    lblOperator.Text = "+";
+                    txtAnswer.Text = result.ToString();
+                }
+        }
+        //Subtract
+        private void btn_subtract_Click(object sender, EventArgs e)
+        {
+            if (validateInput(ref txtNum1, ref num1))
+                if (validateInput(ref txtNum2, ref num2))
+                {
+                    result = num1 - num2;
+                    lblOperator.Text = "-";
+                    txtAnswer.Text = result.ToString();
+                }
+        }
+        //Multiply
+        private void btn_multiply_Click(object sender, EventArgs e)
+        {
+            if (validateInput(ref txtNum1, ref num1))
+                if (validateInput(ref txtNum2, ref num2))
+                {
+                    result = num1 * num2;
+                    lblOperator.Text = "x";
+                    txtAnswer.Text = result.ToString();
+                 }
+        }
+        //Divide
+        private void btn_divide_Click(object sender, EventArgs e)
+        {
+            if (validateInput(ref txtNum1, ref num1))
+                if (validateInput(ref txtNum2, ref num2))
+                {
+                    // ensure not dividing by zero
+                    if (isZero(num1))
+                    {
+                        result = num1 / num2;
+                        lblOperator.Text = "/";
+                        txtAnswer.Text = result.ToString();
+                    }
+                }          
+        }
+        //Modulus 
+        private void btn_modulus_Click(object sender, EventArgs e)
+        {
+            if (validateInput(ref txtNum1,ref num1))
+                if (validateInput(ref txtNum2,ref num2))
+                {
+                    // ensure not dividing by zero
+                    if (isZero(num2))
+                    {
+                        result = num1 % num2;
+                        lblOperator.Text = "%";
+                        txtAnswer.Text = result.ToString();
+                    }
+                }
+        }
+        //Validate user input as correct type
+        private bool validateInput(ref TextBox txtEntry,ref float value)
+        {
+            if(!float.TryParse(txtEntry.Text, out value))
+            {
+                MessageBox.Show("Number is not valid \nTry again!","Invalid Entry");
+                txtEntry.Clear();
+                txtEntry.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        //Check that input is not 0
+        private bool isZero(float secondNum)
+        {
+            if (secondNum == 0)
+            {
+                MessageBox.Show("Cannot divide by zero - Please re-enter number 2", "Entry Error");
+                txtNum2.SelectAll();
+                txtNum2.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
